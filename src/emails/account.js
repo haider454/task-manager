cf9717c3f8732992b2c42efc7bc8223f44f01419
Sharjeel Haider <haidersharjeel454@gmail.com>
2019-03-29 11:00:07 -0400

    Add email verification

---
+++ src/emails/account.js
@@ -0,0 +1,25 @@
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'haidersharjeel454@gmail.com',
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    })
+}
+
+const sendCancelEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'haidersharjeel454@gmail.com',
+        subject: `Goodbye, ${name}`,
+        text: 'Hope you enjoyed your time with us.'
+    })
+}
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelEmail
+}
